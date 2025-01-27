3d10292dc5a5cf0576b5766faf028f214dfb0bae
dreBo <drebo2324@gmail.com>
2025-01-27 15:21:53 -0300

    Post Entity / PostStatus Enum

---
+++ src/main/java/com/drebo/blog/backend/domain/PostStatus.java
@@ -0,0 +1,5 @@
+package com.drebo.blog.backend.domain;
+
+public enum PostStatus {
+    DRAFT, PUBLISHED
+}
---
+++ src/main/java/com/drebo/blog/backend/domain/entities/Post.java
@@ -0,0 +1,72 @@
+package com.drebo.blog.backend.domain.entities;
+
+import com.drebo.blog.backend.domain.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    //columnDefinition = "TEXT" -> allows column to adapt to text size
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    //represent enum in db as string
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(content, post.content) && status == post.status && Objects.equals(readingTime, post.readingTime) && Objects.equals(createdAt, post.createdAt) && Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, status, readingTime, createdAt, updatedAt);
+    }
+
+    //handle null createdAt / updatedAt
+    //call when entity created
+    @PrePersist
+    protected void onCreate(){
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    //call when entity updated
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
