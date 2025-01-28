0c2c2fffc8cb4e19d89b787dfac32e722a74817c
dreBo <drebo2324@gmail.com>
2025-01-28 19:34:17 -0300

    Loading Users

---
+++ src/main/java/com/drebo/blog/backend/security/BlogUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.drebo.blog.backend.security;
+
+import com.drebo.blog.backend.domain.entities.User;
+import com.drebo.blog.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    //UsernameNotFoundException causes BadCredentialsException
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new BlogUserDetails(user);
+
+    }
+}
