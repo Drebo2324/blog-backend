5ad75489173da57ae3b79c3c577e8ffcdbf3e37c
dreBo <drebo2324@gmail.com>
2025-01-29 00:03:36 -0300

    generate / validate jwt

--- src/main/java/com/drebo/blog/backend/services/implementations/AuthenticationService.java
+++ src/main/java/com/drebo/blog/backend/services/implementations/AuthenticationService.java
@@ -1,7 +1,9 @@
 package com.drebo.blog.backend.services.implementations;
 
 import com.drebo.blog.backend.services.AuthenticationServiceInterface;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,12 +44,29 @@ public class AuthenticationService implements AuthenticationServiceInterface {
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
-                .subject(userDetails.getUsername())
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
-                .claims(claims)
-                .signWith(getSignInKey())
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    //only get username if jwt valid
+    private String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 
     private Key getSignInKey() {
